7180d564a02ac2eb01907a9262ebeadfbd79d78e
Vaishnavi <vaishnavira@cybage.com>
2023-06-13 16:47:20 +0530

    initial commit

---
+++ MachineInfoMVC/Controllers/MachineInfoController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace MachineInfoMVC.Controllers
+
+{
+    public class MachineInfoController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        public MachineInfoController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<IActionResult> Index()
+        {
+
+            var response = await _httpClient.GetAsync("https://localhost:7126/api/MachineInfo");
+            var resultString = await response.Content.ReadAsStringAsync();
+            dynamic result = JsonConvert.DeserializeObject(resultString);
+            return View(result);
+        }
+    }
+}
---
+++ MachineInfoMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MachineInfoMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
